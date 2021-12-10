5ee6d6eddc70dc5f42445dcb43e9cfacc50262cc
abukkapattanam <78103327+abukkapattanam@users.noreply.github.com>
2021-12-10 12:47:51 -0500

    Pushing initial changes for tvOS OTSDK SPM support
    Pushing initial changes for tvOS OTSDK SPM support
    
    - Package.swift file which is the config.
    - LICENSE.txt file which contains OT Licensing.
    - README.md which contains OT SDK related README.

---
+++ Sources/OTPublishersHeadlessSDKtvOS.xcframework/tvos-arm64_x86_64-simulator/OTPublishersHeadlessSDKtvOS.framework/Headers/OTPublishersHeadlessSDKtvOS.h
@@ -0,0 +1,24 @@
+//
+//  OTPublishersHeadlessSDKtvOS.h
+//  OTPublishersHeadlessSDKtvOS
+//
+//  Created by OneTrust on 23/06/20.
+//  Copyright © 2020 OneTrust. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef OTPublishersHeadlessSDKtvOS_h
+#define OTPublishersHeadlessSDKtvOS_h
+#include "TCF2Encoder.h"
+#endif /* OTPublishersHeadlessSDKtvOS_h */
+
+//! Project version number for OTPublishersHeadlessSDKtvOS.
+FOUNDATION_EXPORT double OTPublishersHeadlessSDKtvOSVersionNumber;
+
+//! Project version string for OTPublishersHeadlessSDKtvOS.
+FOUNDATION_EXPORT const unsigned char OTPublishersHeadlessSDKtvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTPublishersHeadlessSDKtvOS/PublicHeader.h>
+
+
